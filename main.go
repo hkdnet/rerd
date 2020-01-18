e65da00d4f2fa0f50b5282360b07a8618d7cee08
hkdnet <satoko.itse@gmail.com>
2020-01-19 00:18:01 +0900

    It works

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -21,13 +22,71 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+type Lexer struct {
+	idx    int
+	tokens []string
+
+	result []Table
+}
+
+func NewLexer(src string) *Lexer {
+	s := strings.ReplaceAll(src, ";", " ;")
+	return &Lexer{
+		idx:    0,
+		tokens: strings.Fields(s),
+	}
+}
+
+type Token struct {
+	identifier string
+
+	lBrace bool
+	rBrace bool
+}
+type Table struct {
+	Name    string
+	Columns []Column
+}
+type Column struct {
+	Name string
+	// type string
+
+	Reference string
+}
+
+func (l *Lexer) Lex(lval *yySymType) int {
+	if l.idx >= len(l.tokens) {
+		return 0
+	}
+	s := l.tokens[l.idx]
+	l.idx++
+
+	if s == "{" {
+		return int('{')
+	} else if s == "}" {
+		return int('}')
+	} else if s == ";" {
+		return int(';')
+	} else {
+		lval.token = Token{identifier: s}
+		return identifier
+	}
+}
+func (l *Lexer) Error(s string) {
+	panic(s)
+}
+
 func run(filename string) error {
 	b, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return errors.Wrap(err, "cannot read file")
 	}
 	s := string(b)
-	fmt.Print(s)
-	_ = yyNewParser()
+	p := yyNewParser()
+	l := NewLexer(s)
+	i := p.Parse(l)
+	fmt.Printf("i = %d\n", i)
+	fmt.Printf("%#v\n", l.result)
 	return nil
 }
--- rerd.y
+++ rerd.y
@@ -2,12 +2,46 @@
 package main
 %}
 %start program
-%token a A
 %union {
-i int32;
+	program []Table
+	token Token
+	tables []Table
+	table Table
+	columns []Column
+	column Column
+}
+%type<program> program
+%type<tables> tables
+%type<table> table
+%type<columns> columns
+%type<column> column
+%token<token> identifier
+%token<token> '{'
+%token<token> '}'
+%token<token> ';'
+%%
+program: tables {
+	$$ = $1
+	yylex.(*Lexer).result = $$
 }
 
-%%
-program:
-  | A
 ;
+tables : { $$ = make([]Table, 0) }
+  | table tables {
+	$$ = append($2, $1)
+  }
+;
+table : identifier '{' columns '}' {
+	$$ = Table { Name: $1.identifier, Columns: $3 }
+}
+;
+columns : { $$ = make([]Column, 0) }
+  | column columns {
+	$$ = append($2, $1)
+  }
+;
+column : identifier ';' {
+	$$ = Column{Name: $1.identifier}
+}
+;
+%%
